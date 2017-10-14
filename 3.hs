c84192f71de4ac6aea41e0ddfb98203eecd6055e
RicardasKazakevicius <ricardaskazakevicius@gmail.com>
2017-10-14 15:13:03 +0300

    3 task Exercise 5 finished

--- 3.hs
+++ 3.hs
@@ -1,9 +1,10 @@
 import Data.Char
 
+-- Exercise 1
 subst :: String -> String -> String -> String
 subst a b c = a
 
-
+-- Exercise 2
 isPalin :: String -> Bool
 isPalin [] = True
 isPalin str
@@ -30,7 +31,7 @@ punctuationNotIncluded (x:xs)
   where 
       punctuation = ['.',',',';','-',':'] ++ [' ','\n','\t']
 
-
+-- Exercise 3
 count :: String -> (Int, Int, Int)
 count [] = (0,0,0)
 count s = (c, w, l)
@@ -62,6 +63,7 @@ noOfLines str = (length newLines) + 1
   where newLines = [xx | xx <- str, xx == '\n']
 
 
+-- Exercise 4
 justify :: String -> Int -> String
 justify s n = init(justify' s n) 
 
@@ -95,23 +97,29 @@ justify' s n
         | n == 0 = x:xs
         | otherwise = getRestStr xs (n-1)
 
-
+-- 5 Exercise
 -- overlaps (Circle 2 (2,2)) (Circle 2 (1,2))
 -- overlaps (Rectangle 2 2 (1,1)) (Rectangle 2 2 (1,1))
 -- overlaps (Circle 2 (2,2)) (Rectangle 2 2 (1,1))
--- overlaps (Rectangle 2 2 (1,1)) (Circle 2 (2,2))
+-- overlaps (Rectangle 2 2 (2,2)) (Circle 2 (2,2))
 data Shape = Circle Float (Int,Int) | Rectangle Float Float (Int,Int) 
   deriving (Show, Ord, Eq)
 
 overlaps :: Shape -> Shape -> Bool
-overlaps (Circle c1 (x1,y1)) (Circle c2 (x2,y2)) = area (Circle c1 (x1,y1)) == area (Circle c2 (x2,y2))
-overlaps (Rectangle h1 w1 (x1,y1)) (Rectangle h2 w2 (x2,y2)) = area (Rectangle h1 w1 (x1,y1)) == area (Rectangle h2 w2 (x2,y2))
-overlaps (Circle c (x1,y1)) (Rectangle h w (x2,y2)) = True
-overlaps (Rectangle h w (x1,y1)) (Circle c (x2,y2)) = True
+overlaps (Circle c1 (x1,y1)) (Circle c2 (x2,y2)) = compares (coord(Circle c1 (x1,y1))) (coord(Circle c2 (x2,y2)))
+overlaps (Rectangle w1 h1 (x1,y1)) (Rectangle w2 h2 (x2,y2)) =  compares (coord(Rectangle w1 h1 (x1,y1)))  (coord(Rectangle w2 h2 (x2,y2)))
+overlaps (Circle c (x1,y1)) (Rectangle w h (x2,y2)) = compares (coord(Circle c (x1,y1))) (coord(Rectangle w h (x2,y2)))
+overlaps (Rectangle w h (x1,y1)) (Circle c (x2,y2)) = compares (coord(Rectangle w h (x1,y1))) (coord(Circle c (x2,y2)))
+ 
+compares :: (Float,Float,Float,Float) -> (Float,Float,Float,Float) -> Bool
+compares (x1,x2,y1,y2) (xx1,xx2,yy1,yy2) 
+  | (x1 > xx1 && x1 < xx2 && y1 > yy1 && y1 < yy2) || (x2 > xx1 && x2 < xx2 && y2 > yy1 && y2 < yy2) = True
+  | otherwise = False
 
-area :: Shape -> Float
-area (Circle r (x,y)) = pi*r*r
-area (Rectangle h w (x,y)) = h*w
+coord :: Shape -> (Float,Float,Float,Float)
+coord (Rectangle w h (x,y)) = ((fromIntegral x), (fromIntegral x)+w, (fromIntegral y), (fromIntegral y)+h)
+coord (Circle r (x,y)) = ((fromIntegral x)-r, (fromIntegral x)+r, (fromIntegral y)-r, (fromIntegral y)+r)
 
 
+-- Exercise 6
 --loan :: Person -> Book -> ([??],[???]) -> ([??],[???])
